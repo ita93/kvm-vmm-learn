a915ae188aae27811c46bc399320ed9cc4040640
Nguyen Dinh Phi <phind.uet@gmail.com>
2024-07-28 23:41:01 +0800

    Virtio-blk code
    Signed-off-by: Nguyen Dinh Phi <phind.uet@gmail.com>

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ else
     VECHO = @printf
 endif
 
-OBJS := serial.o vm.o kvm-cmd.o pci.o virtq.o diskimg.o
+OBJS := serial.o vm.o kvm-cmd.o pci.o virtq.o diskimg.o virtio-pci.o virtio-blk.o
 OBJS := $(addprefix $(OUT)/,$(OBJS))
 deps := $(OBJS:%.o=%.o.d)
 
--- src/vm.h
+++ src/vm.h
@@ -21,6 +21,13 @@ int vm_load_image(vm_t *v, const char *image_path);
 int vm_load_initrd(vm_t *v, const char *initrd_path);
 int vm_run(vm_t *v);
 int vm_irq_line(vm_t *v, int irq, int level);
+void *vm_guest_to_host(vm_t *v, void *guest);
+void vm_irqfd_register(vm_t *v, int fd, int gsi, int flags);
+void vm_ioeventfd_register(vm_t *v,
+                           int fd,
+                           unsigned long long addr,
+                           int len,
+                           int flags);
 void vm_exit(vm_t *t);
 
 #endif // VM_H
